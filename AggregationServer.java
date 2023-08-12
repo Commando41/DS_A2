f389d364b376211d378af1dcdb4a39a697f1fd73
Commando41 <50309993+Commando41@users.noreply.github.com>
2023-08-12 16:04:18 +0930

    Created files for aggregation server and GETClient
    Initialized both java files with the appropiate java librarries

---
+++ AggregationServer.java
@@ -0,0 +1,8 @@
+import java.rmi.*;
+import java.io.*;
+import java.net.*;
+
+public class AggregationServer {
+
+    public AggregationServer() {}
+}
---
+++ GETClient.java
@@ -0,0 +1,10 @@
+import java.rmi.*;
+import java.io.*;
+import java.net.*;
+
+public class GETClient {
+
+    public static void main(String[] args) {
+
+    }
+}
