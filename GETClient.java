9098836229ae59168a5ec01c737b06f127600a83
Commando41 <50309993+Commando41@users.noreply.github.com>
2023-09-03 12:00:59 +0930

    Setup the GET Client file
    Added the necessary setup for socket programming!

--- GETClient.java
+++ GETClient.java
@@ -1,10 +1,37 @@
-import java.rmi.*;
 import java.io.*;
 import java.net.*;
+import java.lang.*;
 
 public class GETClient {
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
 
     public static void main(String[] args) {
+        index i;
 
+        try {
+            i = 6;
+            while(!args[i].equals(":")) { i++; }
+                
+            string sPort;
+            while(Character.isDigit(args[++i])) { sPort = sPort + args[i]; }
+                
+
+            socket = new socket(args, Integer.parseInt(sPort));
+            System.out.println("GET client Connected!");
+
+            input = new DataInputStream(System.in);
+            out = new DataOutputStream(socket.getOutputStream());
+
+        }catch (Exception e) {
+            System.out.println(e);
+            return;
+        }
+
+
+        input.close();
+        out.close();
+        socket.close();
     }
 }
